67c6e07f7448ffffac7ce9a5571dde8ca9d268fa
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-08-17 21:47:25 -0300

    Adding the listener class

---
+++ src/listener/listener.py
@@ -0,0 +1,42 @@
+import speech_recognition as sr
+
+
+class Listener():
+    """Responsible by listen everything that the user says
+
+    Args:
+        device_index (int): the microphone index in the OS.
+        noiseless (bool, optional): if the audio recognition should be noiseless or not. Defaults to True.
+    """
+
+    def __init__(self, device_index: int, noiseless: bool = True):
+
+        self.device_index = device_index
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone(self.device_index)
+        self.noiseless = noiseless
+
+    def noiseless(self, audio_source: sr.Microphone) -> None:
+        """ Adjusts the ambient noise
+
+        Args:
+            audio_source (sr.Microphone): The microphone source
+        """
+        if self.noiseless:
+            self.recognizer.adjust_for_ambient_noise(audio_source)
+
+    def listen(self) -> str:
+        """Listen for any audio inputed by the user
+
+        Returns:
+            str: the content listened by the google recognizer
+        """
+        with self.microphone as source:
+            self.noiseless(source)
+            audio = self.recognizer.listen(source)
+        try:
+            content = self.recognizer.recognize_google(audio, language='pt-BR')
+        except sr.UnknownValueError:
+            content = ""
+
+        return content
