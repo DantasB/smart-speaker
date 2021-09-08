bd7c0afd1407f05a0b1166fddfc97ec91e6d8925
Pedro Boechat <pedroboechat@poli.ufrj.br>
2021-09-08 20:37:02 -0300

    Adding activation word

--- src/main.py
+++ src/main.py
@@ -2,6 +2,13 @@ from shared_library.shared_library import generate_workfolder, delete_workfolder
 from listener.listener import Listener
 from speaker.speaker import Speaker
 
+import os
+import dotenv
+dotenv.load_dotenv()
+
+ACTIVATION_WORD = os.environ.get("ACTIVATION_WORD")
+
+print("ACTIVATION WORD:", ACTIVATION_WORD)
 
 if __name__ == "__main__":
     workfolder = generate_workfolder()
@@ -10,7 +17,10 @@ if __name__ == "__main__":
 
     while True:
         content = listener.listen()
-        if content:
+        print('>', content)
+        if content.startswith(ACTIVATION_WORD):
+            speaker.play_audio(speaker.create_audio("como posso ajudar?"))
             speaker.play_audio(speaker.create_audio(content))
             break
     delete_workfolder(workfolder)
+        
